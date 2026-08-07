eccf17a09efd6081c0eb14d63c6b02c8122dca47
Rohman-WG <mrohmannurm2@gmail.com>
2026-08-07 09:11:23 +0700

    Test

--- src/03-conditions/tasks/task-05.ts
+++ src/03-conditions/tasks/task-05.ts
@@ -33,32 +33,39 @@
  *  - Display the correct result.
  */
 
-const studentName = "Fajar Hidayat";
-const GPA = 3.86;
-const familyIncome = 4200000;
-const competitions = 4;
-const isHasDisRecord = false;
-const documentsComplete = true;
 
-const passedFirstScreening = 
-    GPA >= 3.75 && familyIncome < 5000000;
+const student = [
+    {name: "Hasan Hidayah", GPA: 3.75, familyIncome: 41500000, isHasDisRecord: false, documentsComplete: false, Competition: 4},
+    {name: "Fajar Hidayat", GPA: 3.86, familyIncome: 4200000, isHasDisRecord: false, documentsComplete: true, Competition: 5},
+    {name: "Angelina Joly", GPA: 3.96, familyIncome: 8000000, isHasDIsRecord: false, documentsComplete: true, Competition: 1},
+    {name: "Tom holland", GPA: 4.00, familyIncome: 5000000, isHasDisRecord: true, documentsComplete: false, Competition: 3},
+    {name: "Abdul Muthar", GPA: 3.28, familyIncome:9300000, isHasDisRecord: false, documentsComplete: false, Competition: 3},
+    {name: "Hina almahera", GPA: 3.76, familyIncome: 3700000, isHasDisRecord: true, documentsComplete: true, Competition: 4},
+    {name: "Aksa raden P", GPA: 2.00, familyIncome: 3500000, isHasDIsRecord: false, documentsComplete: true, Competition: 1},
+    {name: "Xiena alvonso", GPA: 3.00, familyIncome: 21150000, isHasDisRecord: false, documentsComplete: false, Competition: 2},
+    {name: "Mahameru Abi", GPA: 3.12, familyIncome: 3200000, isHasDisRecord: true, documentsComplete: true, Competition: 3},
+    {name: "Luca Modric", GPA: 1.32, familyIncome: 7000000, isHasDisRecord: false, documentsComplete: true, Competition: 2}
+];
 
-let result;
-
-if (passedFirstScreening) {
-    const passedSecondScreening = 
-    competitions >= 4 &&
-    isHasDisRecord === false &&
-    documentsComplete === true;
-
-    if (passedSecondScreening) {
-        result = "Scholarship Approved";
+for (let i = 0; i < student.length; i++) {
+    const passedFirstScreening = 
+        student[i].GPA >= 3.75 &&
+        student[i].familyIncome >= 5000000
+    let result;
+    
+    if (passedFirstScreening) {
+        const passedSecondScreening = 
+            student[i].Competition >= 3 &&
+            student[i].isHasDisRecord === false &&
+            student[i].documentsComplete === true;
+        if (!passedSecondScreening) {
+            result = "Scholarship approved";
+        } else {
+            result = "Passed first screening, but failed in second screening"
+        }
     } else {
-        result = "Passed first screening but failed in second screening";
+        result = "Failed first screening";
     }
-} else {
-    result = "Failed first screening";
+    console.log("Student name: ", student[i].name);
+    console.log(result);
 }
-
-console.log("Student name = "+studentName);
-console.log(result);
