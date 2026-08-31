bb26dbd23ef1eea172294af47c97b4d3088e1071
Rohman-WG <mrohmannurm2@gmail.com>
2026-08-31 08:17:09 +0700

    completed task 7 arrays and objects

--- src/07-arrays-and-objects/tasks/task-07.ts
+++ src/07-arrays-and-objects/tasks/task-07.ts
@@ -35,3 +35,30 @@ const orders = [
         ],
     },
 ];
+
+const completedOrders = orders.filter(order => order.status === "completed")
+console.log("COMPLETED ORDERS")
+console.log(completedOrders)
+
+const totalValueOrders = orders.map(order => {const total = order.items.reduce
+    ((sum, item) => sum + item.price * item.quantity, 0)
+    return {customer : order.customer,
+            total : total,
+    }})
+console.log("TOTAL VALUE EACH ORDERS")
+console.log(totalValueOrders)
+
+const highSpender = totalValueOrders.reduce((highest, order) => 
+    order.total > highest.total ? order: highest)
+console.log("HIGHEST SPENDER")
+console.log(highSpender)
+
+const totalRevenue = completedOrders.reduce((sum, order) => 
+    {const orderTotal = order.items.reduce((sum, item) => sum + item.price * item.price, 0)
+    return sum + orderTotal},0)
+console.log("TOTAL REVENUE")
+console.log(totalRevenue)
+
+const purchasedItem = orders.flatMap(order => order.items.map(item => item.product))
+console.log("PURCHASED ITEM")
+console.log(purchasedItem)
